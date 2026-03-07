3d2069fe45ee0d18a7948f800ba9907f4ad03b3a
errynei <its.errynei@gmail.com>
2026-03-07 00:38:22 -0700

    Resetting SOs should work now? Maybe?

--- WinterConflict/Assets/Resources/Scripts/StoryProgressionSystem/StoryFlag.cs
+++ WinterConflict/Assets/Resources/Scripts/StoryProgressionSystem/StoryFlag.cs
@@ -5,6 +5,15 @@ using UnityEngine;
 
 public abstract class StoryFlag : ScriptableObject 
 {
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    static void Initialize()
+    {
+        StoryFlag[] storyFlags = Resources.LoadAll<StoryFlag>("");
+
+        foreach (StoryFlag storyFlag in storyFlags)
+            storyFlag.ResetValue();
+    }
+    public abstract void ResetValue();
     public abstract Type GetFlagType();
 }
 public abstract class StoryFlag<T> : StoryFlag
@@ -13,22 +22,23 @@ public abstract class StoryFlag<T> : StoryFlag
     { 
         get
         {
-            return _value;
+            return _currentValue;
         }
         set
         {
             if (logOnValueChanged) 
-                Debug.Log($"StoryFlag {name} changed : {_value} -> {value}", this);
+                Debug.Log($"StoryFlag {name} changed : {_currentValue} -> {value}", this);
 
-            _value = value;
+            _currentValue = value;
         }
     }
-
-    [SerializeField] private T _value;
+    [SerializeField] private T _defaultValue;
+    [SerializeField] private T _currentValue;
     [SerializeField] private bool logOnValueChanged = false;
 
     public override Type GetFlagType() => typeof(T);
+    public override void ResetValue() => _currentValue = _defaultValue;
 
-    public static implicit operator T(StoryFlag<T> storyFlag) => storyFlag._value;
-    protected T Set(T newValue) => _value = newValue;
+    public static implicit operator T(StoryFlag<T> storyFlag) => storyFlag._currentValue;
+    protected T Set(T newValue) => _currentValue = newValue;
 }
