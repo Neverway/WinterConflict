dc68a9dc9fadd5c8646de71ad45f70374e3db04f
errynei <its.errynei@gmail.com>
2026-03-10 09:58:15 -0700

    messing with nodes

---
+++ WinterConflict/Assets/Resources/Scripts/ErryneiTestEnvironment/ErryneiXNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XNode;
+
+public class ErryneiXNode : Node {
+
+	[Input] public SomeClass someInput;
+	[Output] public SomeClass someOutput;
+
+	[Serializable]
+	public class SomeClass
+	{
+		public float x;
+		public float y;
+	}
+
+	// Use this for initialization
+	protected override void Init() {
+		base.Init();
+		
+	}
+
+	// Return the correct value of an output port when requested
+	public override object GetValue(NodePort port) {
+		return null; // Replace this
+	}
+}
+
+public class ErryneiXNode2 : ErryneiXNode
+{
+	public string otherField;
+	public GameObject obj;
+}
