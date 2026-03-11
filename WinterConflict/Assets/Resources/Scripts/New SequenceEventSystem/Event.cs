a854bf858b88ab87f28f0efd43122571a67d1b27
errynei <its.errynei@gmail.com>
2026-03-10 22:40:21 -0700

    Yippie! Buttons!

--- WinterConflict/Assets/Resources/Scripts/New SequenceEventSystem/Event.cs
+++ WinterConflict/Assets/Resources/Scripts/New SequenceEventSystem/Event.cs
@@ -1,19 +1,32 @@
+using ErryLib.Reflection;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.Events;
 
 [Serializable]
 public abstract class Event : IEnumerable<EventSequence.Instruction>
 {
+
     [Tooltip("This is just so you can see stuff in the inspector")]
     public string eventDescription;
+
+    public MethodButton someExampleButton = "Example_Button";
+
+    [ReferenceTag("Example_Button")]
+    public void SomeMethod()
+    {
+        Debug.Log("Trying to invoke!!!!");
+    }
 
     /// <summary> Call this event </summary>
     /// <returns>Returns true if calling the event succeed</returns>
     public abstract IEnumerator<EventSequence.Instruction> Call();
 
+    
 
     //Implementation for IEnumerable (This just lets you enumerate through the.. 
     //  ..Call() function such as with a foreach loop)
